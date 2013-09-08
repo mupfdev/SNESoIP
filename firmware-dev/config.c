2a3838364832e93ed909c8143e11c793d86e7ed2
Michael Fitzmayer <mail@michael-fitzmayer.de>
2013-09-08 11:28:43 +0200

    Added missing counter.

--- firmware-dev/config.c
+++ firmware-dev/config.c
@@ -15,9 +15,10 @@ void readConfig() {
 	int   i = 0, offset;
 
 
-	while (buffer[i] != '\0')
+	while (buffer[i] != '\0') {
 		buffer[i] = eeprom_read_byte((uint8_t*)i);
-
+		i++;
+	}
 
 	p2cidPos = strstr(buffer, "P2CID");
 	if (p2cidPos != NULL) {
@@ -41,14 +42,16 @@ void readConfig() {
 
 
 void writeConfig(char *config) {
-	int i;
+	int i = 0;
 
 	if (
 		(strstr(config, "P2CID") != NULL) &&
 		(strstr(config, "VHOST") != NULL)) {
 
-		while (config[i] != '\0')
+		while (config[i] != '\0') {
 			eeprom_update_byte((uint8_t*)i, config[i]);
+			i++;
+		}
 
 		eeprom_update_byte((uint8_t*)i + 1, '\0');
 	}
