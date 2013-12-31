328ecda4a6d41f2ea482c6c199e54ae1c7f9fac7
Michael Fitzmayer <mail@michael-fitzmayer.de>
2013-12-31 02:51:25 +0100

    Removed testing folder.

--- testing/archive/testrom1/compiler.bat
+++
@@ -1,5 +0,0 @@
-del testrom1.sfc
-wla-65816.exe -vo testrom1.asm testrom1.o
-wlalink.exe -vr testrom1.prj testrom1.sfc
-del testrom1.o
-pause
--- testing/archive/utils/config/README.md
+++
@@ -1,4 +0,0 @@
-# SNESoIP configuration utility #
-
-This tool is not usable at the moment.  The firmware implementation is
-coming soon.
