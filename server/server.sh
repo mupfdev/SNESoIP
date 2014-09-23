8127184503596a22bd62ec6e63930d9ee44cb134
Michael Fitzmayer <mail@michael-fitzmayer.de>
2014-09-23 11:46:14 +0200

    Minor fix.

--- server/server.sh
+++ server/server.sh
@@ -2,6 +2,6 @@
 
 killall socat
 socat -v -T30 \
-	UDP-LISTEN:`echo $(awk -F "= " '/^port/ {print $2}' server.conf)`,fork \
-	TCP:localhost:`echo $(awk -F "= " '/^gwport/ {print $2}' server.conf)` &
+	UDP-LISTEN:`echo $(awk -F "= " '/^gwport/ {print $2}' server.conf)`,fork \
+	TCP:localhost:`echo $(awk -F "= " '/^port/ {print $2}' server.conf)` &
 ./server
